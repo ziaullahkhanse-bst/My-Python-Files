2f39b9f8742dc63b6bbd2496efb3a75f0f0e4d2f
Ziaullah Khan <ziaullahkhan.se@gmail.com>
2026-09-06 14:25:31 +0500

    Fix formatting and clean up comments in tribonacci_sequence.py

--- tribonacci_sequence.py
+++ tribonacci_sequence.py
@@ -7,10 +7,10 @@ def tribonacci(signature, n):
         next_num = result[-1] + result[-2] + result[-3]
         result.append(next_num)
     
-    # Return first n numbers
+    
     return result[:n]
 
-# Test
+
 print(tribonacci([1, 1, 1], 10))  # [1, 1, 1, 3, 5, 9, 17, 31, 57, 105]
 print(tribonacci([0, 0, 1], 10))  # [0, 0, 1, 1, 2, 4, 7, 13, 24, 44]
 print(tribonacci([1, 2, 3], 5))   # [1, 2, 3, 6, 11]
