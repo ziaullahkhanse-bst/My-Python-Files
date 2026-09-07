209f63e6c2109368f11347d73fe7cb0e4eaeb220
Zia <ziaullahkhan.se@gmail.com>
2026-09-07 18:26:38 +0500

    done

---
+++ sum_of_odd.py
@@ -0,0 +1,8 @@
+def row_sum_odd_numbers(n):
+    return n ** 3
+
+print(row_sum_odd_numbers(1))
+print(row_sum_odd_numbers(2))
+print(row_sum_odd_numbers(3))
+print(row_sum_odd_numbers(4))
+print(row_sum_odd_numbers(5))
