cba5afc90f3ab619c0bf2213241e79197cbe9f4a
Zia <ziaullahkhan.se@gmail.com>
2026-09-10 19:32:31 +0500

    done

---
+++ remove_string_spaces.py
@@ -0,0 +1,6 @@
+def no_space(x):
+    return x.replace(" ", "")
+
+print(no_space("8 j 8   mBliB8g  imjB8B8  jl  B"))
+print(no_space("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"))
+print(no_space("8aaaaa dddd r     "))
