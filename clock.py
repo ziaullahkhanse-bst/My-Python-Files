fc14e39d94f20ed8989c7ebd4e917642061541af
Ziaullah Khan <ziaullahkhan.se@gmail.com>
2026-09-10 19:41:06 +0500

    Remove commented test case for past function

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ def past(h, m, s):
     return total_milliseconds
 
 # Test
-print(past(0, 1, 1))    # 61000
+print(past(0, 1, 1))    
 print(past(1, 0, 0))    # 3600000
 print(past(0, 0, 1))    # 1000
 print(past(1, 30, 0))   # 5400000
