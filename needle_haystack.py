4f5a559276fa7f517a63813ac3b17c3993732f29
Ziaullah Khan <ziaullahkhan.se@gmail.com>
2026-09-16 20:20:58 +0500

    Update test case comment for clarity

--- needle_haystack.py
+++ needle_haystack.py
@@ -2,7 +2,7 @@ def find_needle(haystack):
     index = haystack.index("needle")
     return f"found the needle at position {index}"
 
-# Test cases
+# Test 
 print(find_needle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]))
 # Output: found the needle at position 5
 
