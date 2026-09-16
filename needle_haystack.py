ac4847885d99253737a416991ad10dc51290a5b1
Ziaullah Khan <ziaullahkhan.se@gmail.com>
2026-09-16 20:21:38 +0500

    Fix output comment in needle_haystack.py
    Update output comment to match function behavior.

--- needle_haystack.py
+++ needle_haystack.py
@@ -3,7 +3,7 @@ def find_needle(haystack):
     return f"found the needle at position {index}"
 
 print(find_needle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]))
-# Output: found the needle at 
+# Output: found the 
 
 print(find_needle(["needle", "hay", "junk"]))
 # Output: found the needle at position 0
