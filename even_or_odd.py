987eb8d4bd9662021c055ca0013bf63e28f1c1db
Zia <ziaullahkhan.se@gmail.com>
2026-09-23 20:20:35 +0500

    done

---
+++ even_or_odd.py
@@ -0,0 +1,11 @@
+def even_or_odd(number):
+    if number%2==0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+print(even_or_odd(3))
+print(even_or_odd(6))
+print(even_or_odd(39))
+print(even_or_odd(24))
